56429d601b1144fd632ce62caca41200c5f0f76d
Stephen Powers <Stephen@cloudpoweredtech.com>
2020-02-06 16:11:15 -0700

    Still trouble shooting

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,30 +6,10 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "UA-157975049-1" // Google Analytics / GA
-        //  "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-        //  "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: false,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: false,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-        },
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-157975049-1",
       },
     },
     {
